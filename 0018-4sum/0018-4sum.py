796ce0efd4f170ef73b0260cd78f51b4c03453a2
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2023-10-02 20:26:09 +0530

    Time: 718 ms (52.15%), Space: 16.3 MB (58.86%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,17 +1,25 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n=len(nums)
-        st=set()
+        ans=[]
+        nums.sort()
         for i in range(n):
+            if i>0 and nums[i]==nums[i-1]: continue
             for j in range(i+1,n):
-                hashset=set()
-                for k in range(j+1,n):
-                    sum=nums[i]+nums[j]+nums[k]
-                    fourth=target-sum
-                    if fourth in hashset:
-                        temp=[nums[i],nums[j],nums[k],fourth]
-                        temp.sort()
-                        st.add(tuple(temp))
-                    hashset.add(nums[k])
-        ans=list(st)
+                if j>i+1 and nums[j]==nums[j-1]: continue
+                k=j+1
+                l=n-1
+                while k<l:
+                    sum=nums[i]+nums[j]+nums[k]+nums[l]
+                    if sum==target:
+                        temp=[nums[i],nums[j],nums[k],nums[l]]
+                        ans.append(temp)
+                        k+=1
+                        l-=1
+                        while k<l and nums[k]==nums[k-1]: k+=1
+                        while k<l and nums[l]==nums[l+1]: l-=1
+                    elif sum<target:
+                        k+=1
+                    else:
+                        l-=1
         return ans
